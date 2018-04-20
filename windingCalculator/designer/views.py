74d4f85ec1de35d1ee23cb3305d74b395db52345
John Wilkinson <john.wilkinson@fotechsolutions.com>
2018-04-20 12:31:24 +0100

    start of template

--- windingCalculator/designer/views.py
+++ windingCalculator/designer/views.py
@@ -7,5 +7,12 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+#def index(request):
+#    return HttpResponse("Hello, world. You're at the designer index.")
+
+from .models import Wire
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the designer index.")
+    wire_list = Wire.objects.all()
+    context = {'wire_list': wire_list}
+    return render(request, 'designer/index.html', context)
--- windingCalculator/windingCalculator/settings.py
+++ windingCalculator/windingCalculator/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'designer.apps.DesignerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'designer.apps.DesignerConfig',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'windingCalculator.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["/home/fotech/git/primaryWindingsCalculator/windingCalculator/designer/template/designer/"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

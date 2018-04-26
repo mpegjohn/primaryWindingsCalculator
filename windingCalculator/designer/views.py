4fa28216195dc75430e31322c085eb430bc471ea
mpegjohn <john_wilkinson3@sky.com>
2018-04-26 21:42:27 +0100

    Density is working

--- windingCalculator/designer/views.py
+++ windingCalculator/designer/views.py
@@ -29,6 +29,8 @@ def index(request):
 
 
 def wire_size(request):
+    wire_list = Wire.objects.all()
+
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
@@ -40,7 +42,22 @@ def wire_size(request):
             # redirect to a new URL:
             return HttpResponseRedirect('/designer/wire/')
 
-    # if a GET (or any other method) we'll create a blank form
-    form = WireSizeForm()
-    return render(request, 'designer/wire_size.html', {'form': form})
+    else:
+        # if a GET (or any other method) we'll create a blank form
+
+        form = WireSizeForm()
+
+        wire_data=[]
+
+        for wire in wire_list:
+            data = {'diameter': wire.diameter,
+                 'grade_1_dia_max': wire.grade_1_dia_max,
+                 'grade_2_dia_max': wire.grade_2_dia_max,
+                 'current_capacity': wire.current_capacity(3.0),}
+            wire_data.append(data)
+
+
+        context = {'form': form, 'wire_list': wire_data, 'density' : 3.0}
+
+        return render(request, 'designer/wire_size.html', context)
 
